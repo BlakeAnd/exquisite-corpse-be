8d935d6dc4bef13630abd08e5b6654a4fd224b92
Blake Mitchell <mblakeandrew@gmail.com>
2020-04-02 14:54:20 -0700

    debugging logic for combining canvases

--- index.js
+++ index.js
@@ -21,16 +21,44 @@ server.get('/', (req, res) => {
 });
 
 server.post('/drawings', (req, res) => {
+  let drawings_obj = {};
   let id = req.body.pair_id;
   let image_data = req.body.img_data;
   let canvas = req.body.selected_canvas;
+  let canvases_arr = [null, null];
+
+  if(drawings_obj.id === undefined){
+    drawings_obj.id = canvases_arr;
+    if(canvas === "top"){
+      drawings_obj.id[0] = image_data;
+    } else{
+      drawings_obj.id[1] = image_data;
+    }
+    // res.send(drawings_obj.id);
+  }
+  else if (drawings_obj.id[0] === null && canvas === "top"){
+    drawings_obj.id[0] = image_data;
+    respond();
+  }
+  else if (drawings_obj.id[1] === null && canvas === "bottom"){
+    drawings_obj.id[1] = image_data;
+    responder();
+  }
+  function responder() {
+    console.log("res func");
+    let combined_image = drawings_obj.id[0] + drawings_obj.id[1];
+    res.send(combined_image);
+  }
   console.log("\n")
-  // console.log("body", req.body);  
+  // console.log("body", req.body); 
+  console.log("draw obj 0", typeof(drawings_obj.id[0])); 
+  console.log("draw obj 1", typeof(drawings_obj.id[1])); 
   console.log("id ", id);
+  // console.log("obj.id", drawings_obj.id);
   // console.log("img data len", image_data);
   // console.log("canvas ", canvas);
   // console.log(`image_data: ${image_data}`);
-  res.send(req.body);
+  // res.send(req.body);
 })
 
 
