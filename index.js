6efdcd68d1b8f1e5a03066ac6b7cb8fec30ef9e3
Blake Mitchell <mblakeandrew@gmail.com>
2020-07-27 14:35:04 -0700

    changed cors to have url list in one set of quotes

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ server.use(helmet());
 server.use(express.json({limit: '20MB'}));
 
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000", "https://drawexquisitecorpse.netlify.app", "https://drawexquisitecorpse.netlify.com");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000, https://drawexquisitecorpse.netlify.app/, https://drawexquisitecorpse.netlify.com");
   next();
 });
 
